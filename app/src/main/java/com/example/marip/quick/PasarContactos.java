4517b04aa6f344d251d15a84d66b9af86bf5dcc6
Evelyn Castilla <beeve3108@gmail.com>
2019-11-05 20:35:21 -0600

    VISTA METODOS DE CONTACTO

---
+++ app/src/main/java/com/example/marip/quick/PasarContactos.java
@@ -0,0 +1,40 @@
+package com.example.marip.quick;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PasarContactos extends AppCompatActivity {
+
+    Button btnManual, btnAgenda;
+    static final int PICK_CONTACT_RESQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasar_contactos);
+
+        btnManual = (Button) findViewById(R.id.btnManual);
+
+
+        btnManual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Contactos = new Intent(PasarContactos.this, contactos.class);
+                startActivity(Contactos);
+            }
+        });
+
+        btnAgenda = (Button) findViewById(R.id.botonAgenda);
+        btnAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //seleccionarContacto();
+                // finish(); //aqui*****
+
+            }
+        });
+    }
+}
